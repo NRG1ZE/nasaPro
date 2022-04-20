ef1be84bdca15fbdf64666f34ac6d64678f73432
Austin Sianis <96740003+NRG1ZE@users.noreply.github.com>
2022-04-20 17:45:21 -0400

    Commit Landing Request Int

--- landingbackground.js
+++ landingbackground.js
@@ -1,25 +1,29 @@
-const onLoad = {
-  renderBackGround(Image) {
-    const imgSrc = Image.poster === "N/A" ? "" : Image.Poster;
-    return `
-      <img src="${imgSrc}"/>
-      ${Image.Title} (${Image.Year})
-    `;
-  },
+/*
+landing Background
+  This file sets the background of the HTML docment.body to the Astronomy Picture of the Day
+@ params todaysDate
+*/
+let setBackground = document.body.style.background;
+var today = new Date();
+const [YYYY, MM, DD] = [
+  today.getFullYear(),
+  ("0" + (today.getMonth() + 1)).slice(-2),
+  today.getDate(),
+];
+let Return = `${YYYY}` + "-" + `${MM}` + "-" + `${DD}`;
 
-  inputValue(Image) {
-    return Image.Title;
-  },
-  async fetchData(searchTerm) {
+const onLoad = {
+  async fetchData() {
     const response = await axios.get("https://api.nasa.gov/planetary/apod", {
       params: {
         apikey: "MrrxfBkRraKb7d6DrQd6eShpll6SJFmELArFjzP6",
-        s: searchTerm,
+        date: `${Return}`,
       },
     });
 
     if (response.data.Error) return [];
 
-    return response.data.Search;
+    return console.log(response.data);
+    //return setBackGround = `${response.data.imageurl}`
   },
 };
