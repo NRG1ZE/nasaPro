41a8630b834dd8e509d3f2f532d6c0ee3152eea3
Austin Sianis <96740003+NRG1ZE@users.noreply.github.com>
2022-04-22 14:47:59 -0400

    Commit Edits

--- landingbackground.js
+++ landingbackground.js
@@ -6,13 +6,13 @@ Example request:
 https://api.nasa.gov/planetary/apod?api_key=SMcnNJzngrlKyVH47b06X49rp9kacFz3A8xqmdtp
 Debug-https://api.nasa.gov/planetary/apod?date=2022-04-21&apikey=SMcnNJzngrlKyVH47b06X49rp9kacFz3A8xqmdtp
 */
-let background = document.querySelector("img");
+//let background = document.querySelector("img");
 // background.style.minHeight = window.innerHeight;
 // background.style.minWidth = window.innerWidth;
 let imageToFill = document.querySelector("img");
 imageToFill.style.minHeight = window.innerHeight;
 imageToFill.style.minWidth = window.innerWidth;
-imageToFill.style.objectFit = "contain";
+imageToFill.style.aspectRatio = "auto";
 //  fill- This is default. The image is resized to fill the given dimension. If necessary, the image will be stretched or squished to fit
 // contain - The image keeps its aspect ratio, but is resized to fit within the given dimension
 // cover - The image keeps its aspect ratio and fills the given dimension. The image will be clipped to fit
@@ -39,9 +39,12 @@ const fetchData = async () => {
 
   //if (response.data.Error) return [];
   imageToFill.src = response.data.url;
-  console.log(response.data.url);
+  console.log(response.data);
 };
 fetchData();
 
 // capture = https://apod.nasa.gov/apod/image/2204/Apollo-16-station-10crop1110.jpg
 //url("images/avatar.svg")
+/*
+Further objective to establish responsiveness for the primary structur of the Document
+*/
