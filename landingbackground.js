78e97c4bb928c13e5b2cff5dfc6a3edb596b7893
Austin Sianis <96740003+NRG1ZE@users.noreply.github.com>
2022-05-03 11:30:57 -0400

    Commit Save Axios Return without Annotations

--- landingbackground.js
+++ landingbackground.js
@@ -6,49 +6,8 @@ Example request:
 https://api.nasa.gov/planetary/apod?api_key=SMcnNJzngrlKyVH47b06X49rp9kacFz3A8xqmdtp
 Debug-https://api.nasa.gov/planetary/apod?date=2022-04-21&apikey=SMcnNJzngrlKyVH47b06X49rp9kacFz3A8xqmdtp
 */
-
-// Create a match function
-// function mViewport(x) {
-//   if (x.matches) {
-//     document
-//       .getElementById("myViewport")
-//       .setAttribute("content", "width=device-width, height=device-height");
-//   }
-// }
-
-// // // Create a MediaQueryList object
-// const viewportShift = this.matchMedia("(max-width: 700px)");
-
-// // Call the match function at run time:
-// mViewport(viewportShift);
-
+const hero = document.getElementById("searchbarContainer");
 const website = document.body;
-
-const form = `<div type="form" class="searchInput">
-                <form id="keywordSearchBar">
-                  <input id="keywordInput" type="text" class="" />
-                   <button id="keywordButton"></button>
-                </form>
-              </div>`;
-// const webBody = document.getElementById("bodyCenter");
-// let height = window.innerHeight;
-// let width = window.innerWidth;
-// webBody.height = height * 0.7;
-//let background = document.querySelector("img");
-// background.style.minHeight = window.innerHeight;
-// background.style.minWidth = window.innerWidth;
-// let imageToFill = document.querySelector("img");
-// imageToFill.style.minHeight = window.innerHeight;
-// imageToFill.style.minWidth = window.innerWidth;
-
-// imageToFill.style.aspectRatio = "auto";
-//  fill- This is default. The image is resized to fill the given dimension. If necessary, the image will be stretched or squished to fit
-// contain - The image keeps its aspect ratio, but is resized to fit within the given dimension
-// cover - The image keeps its aspect ratio and fills the given dimension. The image will be clipped to fit
-// none - The image is not resized
-// scale-down
-
-//"url('apod.nasa.gov/apod/image/2204/Apollo-16-station-10crop1110.jpg')";
 var today = new Date();
 const [YYYY, MM, DD] = [
   today.getFullYear(),
@@ -68,7 +27,7 @@ const fetchData = async () => {
 
   //if (response.data.Error) return [];
   //imageToFill.src
-  website.style.backgroundImage = `url(${response.data.url})`;
+  website.style.backgroundImage = `url(${response.data.hdurl})`;
   console.log(response.data);
 };
 fetchData();
@@ -78,3 +37,5 @@ fetchData();
 /*
 Further objective to establish responsiveness for the primary structur of the Document
 */
+//https://apod.nasa.gov/apod/image/2205/MercuryTailPleiades_Voltmer_1448.jpg
+//https://apod.nasa.gov/apod/image/2205/MercuryTailPleiades_Voltmer_960_annotated.jpg
