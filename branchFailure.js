a9252c90050b5f46a32aa6eaf72352b0b36e0e6d
Austin Sianis <96740003+NRG1ZE@users.noreply.github.com>
2022-05-05 11:01:24 -0400

    Commit Delete previous branch notes

--- branchFailure.js
+++
@@ -1,6 +0,0 @@
-/*
-Branch - responsivenessTEST
-  Will be testing Bulma CSS as a take over for next testing branch!
-  currently background responsiveness affects the rest of the doc to much
-  refactoring classes may help
-*/
